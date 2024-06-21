ed52737c6957221e0af4f56bbb8cdfc59b10cd56
AsifMohammedSifat <asifmohammedsifat38@gmail.com>
2024-06-21 23:08:57 +0600

    load from db and details page setup

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import in_app
+from .views import in_app,show_objects
 
 urlpatterns = [
     path('',in_app,name="in_app_templates"),
+    path('objects/',show_objects,name="article_objects"),
+    path('objects/<int:id>',show_objects),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import article
 
 # Create your views here.
 
 def in_app(request):
     return render(request,"articles/article_detail.html")
+
+
+def show_objects(request,id=None):
+    context = {}
+    if id is not None:
+        object = article.objects.get(id=id)     
+        context['object'] = object
+    else:
+        objects = article.objects.all()     
+        context['objects'] = objects
+    return render(request,"articles/article_objects.html",context)
