2655be19eda9384ba54daad8c5fe09feda4708b8
AsifMohammedSifat <asifmohammedsifat38@gmail.com>
2024-06-22 00:30:38 +0600

    added search functionality

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import in_app,show_objects
+from .views import in_app,show_objects,search_article
 
 urlpatterns = [
     path('',in_app,name="in_app_templates"),
     path('objects/',show_objects,name="article_objects"),
     path('objects/<int:id>',show_objects),
+    path('search/',search_article,name="search_article"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import article
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -16,3 +17,35 @@ def show_objects(request,id=None):
         objects = article.objects.all()     
         context['objects'] = objects
     return render(request,"articles/article_objects.html",context)
+
+
+def search_article(request,*args, **kwargs):
+    # print(dir(request))
+    context = {"object":None}
+    try:
+        id = int(request.GET.get('q')) 
+    except:
+        id = None
+
+    # print(type(id))
+
+                                # Way-1
+    # if id is not None:
+    #     # object = article.objects.get(id=id)
+    #     object = get_object_or_404(article,id=id)
+    #     context = {
+    #         "object":object
+    #     }
+    # return render(request,"articles/article_search.html",context)
+
+                                # Way-2
+    if id is not None:
+        try:
+            object = article.objects.get(id=id)
+            context = {
+                "object":object
+            }
+        except:
+            return HttpResponse("<h1>No Result Found</h1>")
+            
+    return render(request,"articles/article_search.html",context)
